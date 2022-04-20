f25ccf978b393286b60458f4767946a8595ef302
kapresovsk <kapresovsk@gmail.com>
2022-04-20 15:06:44 +0200

    deto

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({deto: faktjo})
+})
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
