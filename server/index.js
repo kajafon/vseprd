d3b3776d240f85ddc2a0be641ca1b64f90056849
kapresovsk <kapresovsk@gmail.com>
2022-04-20 18:00:56 +0200

    is it possible to ever deploy this shit?

--- server/index.js
+++ server/index.js
@@ -2,23 +2,23 @@
 
 const express = require("express");
 const fs = require('fs')
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
+const path = require("path");
 
-const app = express();
+const app = express()
+
+console.log("---- kokot")
+
+app.get("/corpus", (req, res) => {    
+  let inputText = fs.readFileSync("server/corpus.txt", 'utf-8')
+  res.json({ corpus: inputText, cvd: process.cwd()});
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/", (req, res) => {
-  res.json({deto: "faktjo"})
-})
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-
-app.get("/corpus", (req, res) => {
-    
-    let inputText = fs.readFileSync("server/corpus.txt", 'utf-8')
-    res.json({ corpus: inputText, cvd: process.cwd()});
-  });
- 
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
   
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
