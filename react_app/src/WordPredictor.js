3e3b649ef1edcfbe75fe39bdfa0abc99ef81d925
kapresovsk <kapresovsk@gmail.com>
2022-04-20 08:36:45 +0200

    fixed premise limited always to one char. init corpus from backend/file. discarding unsuficient generated words

--- react_app/src/WordPredictor.js
+++ react_app/src/WordPredictor.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
 // import {Input, Button} from 'antd'
 
 
@@ -13,6 +14,7 @@ const PREMIS_LENGTH = 3
 export default class WordPredictor extends React.Component {
     constructor(props) {
         super(props)
+        
         this.state = {
             prefix: "",
             vocabularyText: "",
@@ -21,17 +23,27 @@ export default class WordPredictor extends React.Component {
             starters: [],
             terminalPredicates: {},
             results:[],
-            numberOfLetters: 5
+            numberOfLetters: 5,
+            showInput: false,
+            error: null
         }
     }
+
+    componentDidMount() {
         
-    process() {
+        fetch("corpus")
+            .then(response => response.json())
+            .then((result)=>this.process(result.corpus))
+        
+    }
+        
+    process(vocabularyText) {
         console.clear()
         console.log("processing vocabulary...")
-        console.log("--->" + this.state.vocabularyText.substring(0,300))
+        console.log("--->" + vocabularyText.substring(0,300))
 
-        let voca = this.state.vocabularyText.replaceAll("."," ").replaceAll(",", " ").replaceAll("\n", " ").replaceAll("'", " ").replaceAll("!", " ")
-        let corpus = voca.split(" ").filter(w => w.length > 1)
+        let voca = vocabularyText.replaceAll("."," ").replaceAll(",", " ").replaceAll("\n", " ").replaceAll("'", " ").replaceAll("!", " ")
+        let corpus = voca.split(" ").filter(w => w.length > 1).map(w => w.toLowerCase())
 
         this.setState({vocabularyText: voca, corpus: corpus})
 
@@ -125,11 +137,15 @@ export default class WordPredictor extends React.Component {
     }
 
     generate(letterCount, prefix) {
-        console.log("==== generate " + letterCount + " letter word.")
-        let resultLetterCount = 0
-        let result = []
+        console.log("\n==== generate " + letterCount + " letter word.")
+        let result = ""
+   
+        let stepsCount = 0
+        let creativity = 0
+        let terminalQuality = 2
 
         let premise = null
+        let prefixMatched = true
 
         if (prefix && this.state.starters.length > 0) {
             let starti = Math.floor(Math.random() * this.state.starters.length)
@@ -153,20 +169,23 @@ export default class WordPredictor extends React.Component {
         if (premise == null){
             let rnd = Math.floor(Math.random() * this.state.starters.length)
             premise = this.state.starters[rnd]
+            prefixMatched = false
             console.log("starter: " + rnd + " -> '" + premise + "'" )
         }
 
-        result.push(premise)
+        result = premise
+        stepsCount++
 
-        while(resultLetterCount < letterCount) {
+        while(result.length < letterCount) {
             let closures = null 
             let isTerminal = false
-            if (resultLetterCount == letterCount -1)
+            if (result.length == letterCount - 1)
             {
                 isTerminal = true
                 closures = this.state.terminalPredicates[premise]    
                 if (!hasValue(closures)) {
                     console.log("~~~ premise '" + premise + "' has no terminal closure")
+                    terminalQuality--
                 }
             } 
             
@@ -177,16 +196,20 @@ export default class WordPredictor extends React.Component {
 
             if (!hasValue(closures)) {
                 console.log("~~~ premise '" + premise + "' has no closure")
+                terminalQuality--
                 break
             }
 
             const rnd = Math.random()
             let followingLetters = Object.keys(closures)
             let candidate = null        
-            console.log( (isTerminal ? "T " : "") + "thrshold: " + rnd + ", option count: " + followingLetters.length)
+            console.log( (isTerminal ? "T " : "") + "{" + premise + "} thrshold: " + rnd + ", option count: " + followingLetters.length)
             let trialCount = 4
             let stepSize = Math.max(1,followingLetters.length / trialCount)
             let getStep = ()=> Math.round(Math.max(1, Math.random()*stepSize * 1.5))            
+
+            creativity += followingLetters.length
+            stepsCount++
 
             for (let i = Math.round(Math.random()*stepSize / 2); i < followingLetters.length; i += getStep()) {
                 let letter = followingLetters[i]
@@ -209,31 +232,79 @@ export default class WordPredictor extends React.Component {
                 throw new Error("no candidate")
             }
 
-            premise = candidate
-            result.push(premise)
-            resultLetterCount += premise.length
+            result += candidate
+            let indx = result.length - PREMIS_LENGTH
+            if (indx < 0) {
+                indx = 0
+            }
+            premise = result.substring(indx, indx + PREMIS_LENGTH)
         }
-        return result.join("")        
+        return {result: result, creativity: creativity / stepsCount, terminalQuality: terminalQuality, prefixMatched: prefixMatched }
     }
 
     onGenerate(count, prefix)
     {
-        let newword = this.generate(count, prefix)
-        console.log("new word (" + count + "): " + newword)
-        this.state.results.push(newword)
-        this.setState({results: this.state.results})
+        this.setState({error: null})
+        for (let i=10; i>0; i--) {
+            let result = this.generate(count, prefix)
+            // console.log("\n")
+            let exist = this.state.corpus.findIndex((val, i) => val === result.result)
+            if (exist >= 0) {
+                console.log(result.result + "---> is in corpus. skipping")
+                continue
+            }
+            exist = this.state.results.findIndex((val, i) => val.result === result.result)
+            if (exist >= 0) {
+                console.log(result.result + "---> is already found. skipping")
+                continue
+            }
+            if (result.terminalQuality < 2) {
+                console.log("---> discarding '" + result.result + "' c:" + result.creativity + ", t:" + result.terminalQuality)
+                continue
+            } 
+            
+            console.log("---> new word (" + count + "): " + result.result)
+            this.state.results.push(result)
+            this.setState({results: this.state.results})
+            return
+        }
+
+        this.setState({error: "Nothing found. Try again ... or something else."})
     }
 
     render() {
 
+        let lastWordGui = null
+        if (this.state.error) {
+            lastWordGui = <h3 style={{color: "red"}}>{this.state.error}</h3>
+        } else if (this.state.results.length > 0) { 
+            let r = this.state.results[this.state.results.length-1]    
+            lastWordGui = (    
+                <div>
+                    <span style={{fontSize: "30px"}}>
+                        {r.result}
+                    </span>
+                    (c:{r.creativity}, t:{r.terminalQuality}, p:{r.prefixMatched.toString()})
+                </div>
+            )
+        }
+
         return (
             <div>
-                <div>
-                    <textarea rows="40" cols="100" value={this.state.vocabularyText} 
-                        onChange={(e)=> this.setState({vocabularyText: e.target.value})}
-                    />
-                </div>
-                <button onClick={()=> this.process()}>Process!</button>
+                {this.state.showInput
+                    ?
+                    <div>
+                        <textarea rows="40" cols="100" value={this.state.vocabularyText} 
+                                onChange={(e)=> {
+                                    this.setState({vocabularyText: e.target.value})
+                                    this.process(e.target.value)
+                                }}
+                        />
+                        <button onClick={()=> this.setState({showInput:false})}>{"<"}</button>
+                    </div>
+                    : <button onClick={()=> this.setState({showInput:true})}>{">"}</button>
+                }
+                
                 <div>
                     {this.state.predicates != null
                     ?
@@ -247,11 +318,37 @@ export default class WordPredictor extends React.Component {
                         null
                     } 
                 </div>
-                <h3 >
-                {
-                    this.state.results.join(", ")
-                }
-                </h3>
+                <br/>
+                <div>
+                    { lastWordGui }
+                    <br/>
+                    <div>
+                        {
+                            this.state.results.length > 1
+                            ?
+                                <div style={{fontSize: "12", color:"grey"}}>Previous results:</div>
+                            :
+                                null
+                        }
+                        {
+                            this.state.results.map((result, i) => {
+                                if (i === this.state.results.length-1) {
+                                    return null
+                                }
+                                return (
+                                    <span key={i} style={{marginRight: "20px"}}>
+                                        <span style={{fontSize: "20px"}}>
+                                            {result.result}
+                                        </span>
+                                        <span style={{marginLeft:"5px"}}>
+                                            (c:{result.creativity}, t:{result.terminalQuality}, p:{result.prefixMatched.toString()})
+                                        </span>
+                                    </span>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 // server/index.js
 
 const express = require("express");
-
+const fs = require('fs')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+  });
+
+app.get("/corpus", (req, res) => {
+    
+    let inputText = fs.readFileSync("server/corpus.txt", 'utf-8')
+    res.json({ corpus: inputText, cvd: process.cwd()});
   });
  
   
